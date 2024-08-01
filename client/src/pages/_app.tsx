0d3d242b809392f37a82546c08004aa90b432754
 <96326525+mutasim77@users.noreply.github.com>
2024-08-01 11:12:53 +0500

    🏗️ refactor: Restructure project and update core files(main pages)

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Poppins } from 'next/font/google';
+import Head from "next/head";
+import "@/styles/globals.css";
+
+const poppins = Poppins({
+  weight: ['400', '500', '700'],
+  style: ['italic', 'normal'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>FastChat - Real-time Scalable Messaging</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={poppins.className}>
+        <Component {...pageProps} />;
+      </main>
+    </>
+  )
 }
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#2a2a2a" />
+        <meta
+          name="description"
+          content="FastChat: Real-time, scalable messaging powered by WebSockets and Redis. Connect instantly and scale effortlessly."
+        />
+        <meta name="keywords" content="chat, real-time, WebSockets, Redis, scalable, messaging" />
+        <meta name="author" content="Mutasim" />
+        <meta property="og:title" content="FastChat - Real-time Scalable Messaging" />
+        <meta property="og:description" content="Experience instant, scalable messaging with FastChat. Powered by WebSockets and Redis." />
+        <meta property="og:type" content="website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
